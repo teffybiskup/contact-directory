7b2289d1fff76b3df2983015921cdf4b7e571de1
Stefany Biskup <33550545+teffybiskup@users.noreply.github.com>
2025-06-05 13:30:20 -0700

    Change App to Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContactDirectory from '../components/ContactDirectory';
+
+function Home() {
+  return (
+    <><ContactDirectory/></>
+  );
+}
+
+export default Home;
